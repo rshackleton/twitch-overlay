6906c9899472a2fd5def17bdebe1bbf88050f11f
Richard Shackleton <rshackleton@live.co.uk>
2017-07-27 21:40:27 +0100

    Fix index path

--- web/app.js
+++ web/app.js
@@ -40,7 +40,7 @@ app.use(morgan('combined', { 'stream': logger.stream }));
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + 'dist/index.html'));
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
 app.listen(PORT, HOST, function () {
